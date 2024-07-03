159f9cb5eea07368cc96107402adccdc07ac858a
Olaf Lüke <olaf@tinkerforge.com>
2024-07-03 21:05:21 +0200

    Add sequences to image test script

--- software/tests/image_to_rgb565.py
+++ software/tests/image_to_rgb565.py
@@ -3,7 +3,7 @@
 
 HOST = "localhost"
 PORT = 4223
-UID = "2ehida" 
+UID = "2ehida"
 
 from tinkerforge.ip_connection import IPConnection
 from tinkerforge.bricklet_warp_front_panel import BrickletWARPFrontPanel
@@ -11,19 +11,20 @@ from tinkerforge.bricklet_warp_front_panel import BrickletWARPFrontPanel
 from PIL import Image
 import time
 
-im = Image.open('doom.png', 'r')
-width, height = im.size
-pixel_values = list(im.getdata())
 pixel_values_565 = []
 pixel_values_565_8bit = []
-for value in pixel_values:
-    r = value[0] >> 3
-    g = value[1] >> 2
-    b = value[2] >> 3
-    rgb565 = (r<<11) | (g<<5) | b
-    pixel_values_565.append(rgb565)
-    pixel_values_565_8bit.append(rgb565 & 0xFF)
-    pixel_values_565_8bit.append(rgb565 >> 8)
+for i in range(2):
+    im = Image.open('warp_seq3/{0}.png'.format(i), 'r')
+    width, height = im.size
+    pixel_values = list(im.getdata())
+    for value in pixel_values:
+        r = value[0] >> 3
+        g = value[1] >> 2
+        b = value[2] >> 3
+        rgb565 = (r<<11) | (g<<5) | b
+        pixel_values_565.append(rgb565)
+        pixel_values_565_8bit.append(rgb565 & 0xFF)
+        pixel_values_565_8bit.append(rgb565 >> 8)
 
 def chunker(seq, size):
     return (seq[pos:pos + size] for pos in range(0, len(seq), size))
@@ -36,11 +37,14 @@ if __name__ == "__main__":
 
     index = 0
     sub_index = 0
+    print("Erase flash: ", wfp.erase_flash())
+    time.sleep(1)
     wfp.set_flash_index(0, 0)
     for index, data in enumerate(chunker(pixel_values_565_8bit, 64)):
         ret = wfp.set_flash_data(data)
         while ret.status != BrickletWARPFrontPanel.FLASH_STATUS_OK:
             print("try again ", index, ret)
             ret = wfp.set_flash_data(data)
+            time.sleep(0.002)
 
 #        print(index, ret)
