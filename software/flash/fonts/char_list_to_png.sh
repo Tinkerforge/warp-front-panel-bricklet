efccfd7af31a5ccfd4d9d6a3dab7edc3ee1a847b
Olaf Lüke <olaf@tinkerforge.com>
2024-07-16 15:01:28 +0200

    Update fonts and use PX instead of PT for defines

--- software/flash/fonts/char_list_to_png.sh
+++ software/flash/fonts/char_list_to_png.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-convert -size 50:3600 xc:white -font "DejaVu-Sans-Mono-Bold" -pointsize 14 -fill black -draw @char_list.txt 0.png
-convert -size 50x3600 xc:black -font "DejaVu-Sans-Mono-Bold" -pointsize 14 -fill white -draw @char_list.txt 1.png
-convert -size 50x3600 xc:'#003067' -font "DejaVu-Sans-Mono-Bold" -pointsize 14 -fill white -draw @char_list.txt 2.png
-convert -size 50x3600 xc:white -font "DejaVu-Sans-Mono-Bold" -pointsize 16 -fill black -draw @char_list.txt 3.png
-convert -size 50x3600 xc:black -font "DejaVu-Sans-Mono-Bold" -pointsize 16 -fill white -draw @char_list.txt 4.png
-convert -size 50x3600 xc:'#003067' -font "DejaVu-Sans-Mono-Bold" -pointsize 16 -fill white -draw @char_list.txt 5.png
-convert -size 50x3600 xc:white -font "DejaVu-Sans-Mono-Bold" -pointsize 18 -fill black -draw @char_list.txt 6.png
-convert -size 50x3600 xc:black -font "DejaVu-Sans-Mono-Bold" -pointsize 18 -fill white -draw @char_list.txt 7.png
-convert -size 50x3600 xc:'#003067' -font "DejaVu-Sans-Mono-Bold" -pointsize 18 -fill white -draw @char_list.txt 8.png
+convert -size 50:3600 xc:white -font "DejaVu-Sans-Mono-Bold" -pointsize 16 -fill black -draw @char_list.txt 0.png
+convert -size 50x3600 xc:black -font "DejaVu-Sans-Mono-Bold" -pointsize 16 -fill white -draw @char_list.txt 1.png
+convert -size 50x3600 xc:'#003067' -font "DejaVu-Sans-Mono-Bold" -pointsize 16 -fill white -draw @char_list.txt 2.png
+convert -size 50x3600 xc:white -font "DejaVu-Sans-Mono-Bold" -pointsize 18 -fill black -draw @char_list.txt 3.png
+convert -size 50x3600 xc:black -font "DejaVu-Sans-Mono-Bold" -pointsize 18 -fill white -draw @char_list.txt 4.png
+convert -size 50x3600 xc:'#003067' -font "DejaVu-Sans-Mono-Bold" -pointsize 18 -fill white -draw @char_list.txt 5.png
+convert -size 50x3600 xc:white -font "DejaVu-Sans-Mono-Bold" -pointsize 20 -fill black -draw @char_list.txt 6.png
+convert -size 50x3600 xc:black -font "DejaVu-Sans-Mono-Bold" -pointsize 20 -fill white -draw @char_list.txt 7.png
+convert -size 50x3600 xc:'#003067' -font "DejaVu-Sans-Mono-Bold" -pointsize 20 -fill white -draw @char_list.txt 8.png
 
 #IFS=" x+" read a b c d < <(convert -fuzz 10% font_16pt_freemono_black_on_white.png -format "%@" info:)
 #convert font_16pt_freemono_black_on_white.png -crop "${a}x+${c}+0" font_16pt_freemono_black_on_white.png
