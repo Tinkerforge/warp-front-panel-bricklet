96ea288104bfab58fd8c36c4f866596953f5c1e8
Erik Fleckstein <erik@tinkerforge.com>
2024-09-13 15:23:20 +0200

    Update sprites.

--- software/flash/defines.py
+++ software/flash/defines.py
@@ -26,6 +26,15 @@ SPRITES = [
     'STATUS_ICON_NO_ETHERNET', # 10.png
     'ICON_HEATING',            # 11.png
     'ICON_CHARGE_MANAGEMENT',  # 12.png
-    'ICON_HOME_METER',         # 13.png
+    'ICON_HEATING_OFF',        # 13.png
     'ICON_EMPTY',              # 14.png
+    'ICON_ENERGY_IMPORT',      # 15.png
+    'ICON_ENERGY_EXPORT',      # 16.png
+    'ICON_HEATING_COLD',       # 17.png
+    'ICON_HEATING_HOT',        # 18.png
+    'ICON_HEATING_HOT_2',      # 19.png
+    'ICON_CLOUD',              # 20.png
+    'ICON_CLOUD_DRIZZLE',      # 21.png
+    'ICON_CLOUD_SUN',          # 22.png
+    'ICON_ENERGY',             # 23.png
 ]
