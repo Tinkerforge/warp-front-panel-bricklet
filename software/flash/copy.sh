e0407923f26ff7565135af762566ab074fc4ee3e
Olaf Lüke <olaf@tinkerforge.com>
2024-09-14 22:55:47 +0200

    Add metadata to flashmap
    Currently includes version, length and checksum, but can
    be extended later on.

--- software/flash/copy.sh
+++ software/flash/copy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp font_defines.h font_list.h sprite_defines.h sprite_list.h ../src/
+cp font_defines.h font_list.h sprite_defines.h sprite_list.h metadata.h ../src/
 cp sprite_defines.h font_defines.h ../../../esp32-firmware/software/src/modules/front_panel/
